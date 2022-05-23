1854ffa45492c46d9d16c820bb15724925ee3ed4
njlr <njlr@users.noreply.github.com>
2022-05-23 08:48:43 +0100

    * Sanity test for Guid

--- tests/Primitives.fs
+++ tests/Primitives.fs
@@ -33,6 +33,13 @@ let tests = testList "Primitives" [
     Expect.equal actual expected "The decoded value must match the original"
   }
 
+  test "Codec.guid works for simple case 1" {
+    let expected = Guid.Parse "11850f17-351a-436c-8358-b28eb85a52a6"
+    let actual = roundTrip Codec.guid expected
+
+    Expect.equal actual expected "The decoded value must match the original"
+  }
+
   test "Codec.datetimeAsIs works for simple case 1" {
     let expected = DateTime.Parse "2022-05-23T07:45:39.700Z"
     let actual = roundTrip Codec.datetimeAsIs expected
