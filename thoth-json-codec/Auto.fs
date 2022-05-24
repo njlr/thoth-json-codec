abc0db2f2c0291f6c1954dcf0d05307cc78fc732
njlr <njlr@users.noreply.github.com>
2022-05-24 08:51:00 +0100

    * Adds inlnie for Fable

--- thoth-json-codec/Auto.fs
+++ thoth-json-codec/Auto.fs
@@ -12,7 +12,7 @@ module Auto =
   module Codec =
 
     type Auto private () =
-      static member generateCodec(?caseStrategy : CaseStrategy, ?extra : ExtraCoders, ?skipNullField : bool) : Codec<'t> =
+      static member inline generateCodec(?caseStrategy : CaseStrategy, ?extra : ExtraCoders, ?skipNullField : bool) : Codec<'t> =
         Codec.create
           (Encode.Auto.generateEncoder(?caseStrategy=caseStrategy, ?extra=extra, ?skipNullField=skipNullField))
           (Decode.Auto.generateDecoder(?caseStrategy=caseStrategy, ?extra=extra))
